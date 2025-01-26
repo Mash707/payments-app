cfbe6dc7eb268b848d747dfc3c67c13d5b7744c1
Divyanshu Gupta <divyanshugupta585@gmail.com>
2025-01-26 18:48:33 +0530

    set the default path to /signin

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import { Signup } from "./pages/Signup";
-import { Signin } from "./pages/SignIn";
-import { Dashboard } from "./pages/DashBoard";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
 import { SendMoney } from "./pages/SendMoney";
 import { PaymentAcknowledgement } from "./pages/PaymentAcknowledgement";
 
@@ -10,6 +10,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Navigate to="/signin" replace />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
           <Route path="/dashboard" element={<Dashboard />} />
