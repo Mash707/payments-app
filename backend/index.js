0853166c5876cdbd2294b3d373e9b6e53466134e
Divyanshu Gupta <divyanshugupta585@gmail.com>
2025-02-15 18:11:22 +0530

    ----

--- backend/index.js
+++ backend/index.js
@@ -2,16 +2,10 @@ const express = require("express");
 const rootRouter = require("./routes/index");
 const cors = require("cors");
 const app = express();
-const serveHandler = require("serve-handler");
-const path = require("path");
 
 app.use(cors());
 app.use(express.json());
+
 app.use("/api/v1", rootRouter);
-app.use((req, res) => {
-  return serveHandler(req, res, {
-    public: path.join(__dirname, "../frontend/dist"),
-  });
-});
 
 app.listen(3000);
